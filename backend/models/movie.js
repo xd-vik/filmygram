23fadd5909cc1ca63bf1fbeb8515bee14503ea5f
SastaStudent <mc1377929@gmail.com>
2025-02-02 21:21:26 +0530

    Auth

--- backend/models/movie.js
+++ backend/models/movie.js
@@ -4,16 +4,16 @@ const connectDB = require('../db/DB_Connect')
 
 connectDB();
 
-const movieSchema = mongoose.Schema({
+const movieSchema = new mongoose.Schema({
     id:{type:Number, unique:true},
     title:String,
     overview:String,
-    img1:String,
-    img2:String,
-    img3:String,
-    dlink1:String,
-    dlink2:String,
-    dlink3:String
+    // img1:String,
+    // img2:String,
+    // img3:String,
+    // dlink1:String,
+    // dlink2:String,
+    // dlink3:String
 })
 
 module.exports = mongoose.model('movieCollections',movieSchema);
